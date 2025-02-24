7cf03526e1a8d74e744a2e8c7dd27eedcf710119
Sreekar-Kandhadai <kandhadaisreekar@gmail.com>
2025-02-24 22:02:50 +0530

    Create Zepto.sql

---
+++ Complex_questions/Zepto.sql
@@ -0,0 +1,26 @@
+The question is you have a table named A where the input is
+
+1
+2
+3
+4
+5
+Now, you need to get the output in the following format
+1
+2
+2
+3
+3
+3
+4
+4
+4
+4
+5
+5
+5
+5
+5
+
+Note: Do not use recursive cte
+
